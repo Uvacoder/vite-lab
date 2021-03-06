a0779497a0a91b77996a28306f4cd48797cce018
Alvaro Saburido <alvaro.saburido@gmail.com>
2021-03-06 20:17:23 +0100

    Auto stash before revert of "feat: added windicss instead of tailwind"

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import Modal from '../views/experiments/Modal.vue';
 import Battery from '../views/experiments/Battery.vue';
+import FancyClock from '../views/experiments/FancyClock.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'Home', component: Home },
   { path: '/modal', name: 'Modal', component: Modal },
   { path: '/battery', name: 'Battery', component: Battery },
+  { path: '/fancy-clock', name: 'Battery', component: FancyClock },
 ];
 
 export default routes;
---
+++ src/views/experiments/FancyClock.vue
@@ -0,0 +1,37 @@
+<script>
+import { defineComponent, computed, ref } from 'vue';
+import { useNow } from '@vueuse/core';
+
+export default defineComponent({
+  name: 'FancyClockExperiment',
+  setup() {
+    const { now } = useNow();
+
+    const hours = computed(() => now.value.getHours());
+    const minutes = computed(() => now.value.getMinutes());
+    return {
+      hours,
+      minutes,
+    };
+  },
+});
+</script>
+
+<template>
+  <div class="app container pt-8">
+    <div
+      class="card mx-auto bg-white shadow-lg rounded-xl p-8 border border-gray-100 w-48"
+    >
+      <div class="clock column">
+        <span
+          class="hours text-gradient bg-gradient-to-t from-red-400 via-orange-500 to-yellow-500"
+          >{{ hours }}</span
+        >
+        <span
+          class="minutes text-gradient bg-gradient-to-t from-green-400 via-blue-500 to-yellow-500"
+          >{{ minutes }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
